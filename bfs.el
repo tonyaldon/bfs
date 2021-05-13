b29feca4b1ea7059b7b4a6f7367b91849000a5b4
tony <tony.aldon.adm@gmail.com>
2021-05-13 18:52:26 +0200

    add package Headers and no error with (package-lint)
    * bfs.el:  Add package Headers and no error with (package-lint).

--- bfs.el
+++ bfs.el
@@ -1,8 +1,31 @@
 ;;; bfs.el --- Browse File System -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2021 Tony Aldon
+
+;; Author: Tony Aldon <tony.aldon.adm@gmail.com>
+;; Version: 0.10.0
+;; Package-Requires: ((emacs "27.1") (dash "2.17.0") (f "0.20.0"))
+;; Keywords: files
+;; Homepage: https://github.com/tonyaldon/bfs
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
+;; TODO:
 
 ;;; Code:
 
